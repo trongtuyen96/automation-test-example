492885851ad608e16f7395c884cbe1708532b223
Tuyen Nguyen <tuyen.nguyen@hansencx.com>
2020-03-08 14:18:14 +0700

    Sample test with Google

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,18 +1,38 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class SampleTest {
-    @Test
-    public void setup() {
-        System.out.println("This is setup test");
+    public String baseURL = "http://google.com";
+    public WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "D:\\Study\\automationFramework\\src\\main\\resources\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get(baseURL);
     }
 
-    @Test
-    public void logIn() {
-        System.out.println("This is log in test");
+    @Test(priority = 0)
+    public void Search() throws InterruptedException {
+        driver.findElement(By.cssSelector("[name='q']")).sendKeys("Automation Test");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[@class='tfB0Bf']//input[@name='btnK']")).click();
     }
 
-    @Test
-    public void logOut() {
-        System.out.println("This is log out test");
+    @Test (priority = 1)
+    public void validateResult() {
+        Assert.assertTrue(driver.findElement(By.xpath("//div[@id='result-stats']")).getText().length() > 0, "In result page");
     }
+
+    @AfterTest
+    public void close() {
+        driver.close();
+    }
+
 }
